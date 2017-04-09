88abd99daab2b64537c1fa4dab50844655ed64cc
sherwy <tanabodee.l@outlook.com>
2017-04-09 16:27:44 +0700

    -Fixed bug (folder didn't create before add files)

--- src/main/java/com/tana/utilities/FolderUtilities.java
+++ src/main/java/com/tana/utilities/FolderUtilities.java
@@ -4,23 +4,25 @@ import java.io.File;
 
 import org.apache.log4j.Logger;
 
-
 public class FolderUtilities {
-	
+
 	public static Logger LOGGER = Logger.getLogger(FolderUtilities.class);
-	
-	public static void createFolderIfNotExist(String path){
-		File theDir = new File(path);
-		if (!theDir.exists()) {
-		    LOGGER.info("Creating directory: " + theDir.getName());
-		    try{
-		        theDir.mkdir();
-		    } 
-		    catch(SecurityException se){
-		    	LOGGER.info("Cann't create the folder : "+path);
-		    }
-		}else{
-			LOGGER.info("Folder is already exist : "+path);
+
+	public static void createFolderIfNotExist(String path) {
+		String[] pathSplit = path.split("/");
+		for (int i = 0; i < pathSplit.length; i++) {
+			path = pathSplit[i];
+			File theDir = new File(path);
+			if (!theDir.exists()) {
+				LOGGER.info("Creating directory: " + theDir.getName());
+				try {
+					theDir.mkdir();
+				} catch (SecurityException se) {
+					LOGGER.info("Cann't create the folder : " + path);
+				}
+			} else {
+				LOGGER.info("Folder is already exist : " + path);
+			}
 		}
 	}
 }
